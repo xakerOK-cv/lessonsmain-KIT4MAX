ac528f87270a26b0a49b318372711bc677df600d
MAX <bodnarykmaxsym@gmail.com>
2025-02-01 17:02:42 +0200

    Complet

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -5,19 +5,35 @@
 # yjdsq
 
 sites = {
-    'Moscow': (550, 370),
+    'Kiev': (550, 370),
     'London': (510, 510),
     'Paris': (480, 480),
 }
 
 # Составим словарь словарей расстояний между ними
 # расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
+distance = dict()
+kiev = sites['Kiev']
+london = sites['London']
+paris = sites['Paris']
+kiev_london = ((kiev[0] - london[0]) ** 2 + (kiev[1] - london[1]) ** 2) ** 0.5
+kiev_london = round(kiev_london, 2)
+kiev_paris = ((kiev[0] - paris[0]) ** 2 + (kiev[1] - paris[1]) ** 2) ** 0.5
+kiev_paris = round(kiev_paris, 2)
+london_paris = ((london[0] - paris[0]) ** 2 + (london[1] - paris[1]) ** 2) ** 0.5
+london_paris = round(london_paris, 2)
 
-distances = {}
+print(kiev_london)
+print(kiev_paris)
 
-# TODO здесь заполнение словаря
+distance['Kiev'] = {}
+distance['Kiev']['London'] = kiev_london
+distance['Kiev']['Paris'] = kiev_paris
 
-print(distances)
+distance['London'] = {}
+distance['London']['Paris'] = london_paris
+
+print(distance)
 
 
 
